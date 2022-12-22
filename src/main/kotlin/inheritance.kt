8c7386ee7acb5054d79b883f970ffae13f20b546
mtheuslipe <matheusfelipe.mf454@gmail.com>
2022-12-22 12:26:23 -0300

    prática de herança com construtor parametrizado

--- src/main/kotlin/inheritance.kt
+++ src/main/kotlin/inheritance.kt
@@ -1,4 +1,4 @@
-//classe pai
+//classe pai para praticar herança simples
 open class Dog(){
     open fun sayHello(){
         println("au au au")
@@ -11,8 +11,22 @@ class Yorkshire: Dog(){
         println("wif wif wif!")
     }
 }
+
+//prática de herança com construtor parametrizado
+open class Car(nome: String){
+    val carName: String = nome
+    open fun sayCarName(){
+        println("the car is a $carName")
+    }
+}
+class Volks : Car("Gol")
+
 fun main(){
-    //chamando classes e testando aplicabilidade da propriedade
+    //chamando classes e testando aplicabilidade da propriedade e vendo como funciona herança simples
     val dog: Dog = Yorkshire()
     dog.sayHello()
+
+    //chamando as classes e trabalhando com herança com construtor parametrizado e também plimorfismo
+    val car: Car = Volks()
+    car.sayCarName()
 }
